769749c765578a33adecc812ba7e7b47007a7cb7
Paras Danidhariya <124392753+Parasss111@users.noreply.github.com>
2026-02-06 12:03:21 +0530

    Update email transport configuration for nodemailer

--- utils/senEmail.js
+++ utils/senEmail.js
@@ -1,12 +1,15 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL,
-    pass: process.env.EMAIL_PASS,
-  },
+    pass: process.env.EMAIL_PASS
+  }
 });
+
 
 const sendEmail = async ({ to, subject, html }) => {
   await transporter.sendMail({
